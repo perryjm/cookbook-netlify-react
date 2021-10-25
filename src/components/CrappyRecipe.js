95f74a353d134529b607a2adbddb927d84b3b62b
Mike Perry <perryjm@users.noreply.github.com>
2021-10-25 00:03:46 -0600

    Update CrappyRecipe.js

--- src/components/CrappyRecipe.js
+++ src/components/CrappyRecipe.js
@@ -4,8 +4,14 @@ const CrappyRecipe = props => {
 	return (
 		<div>
 			<h2>{props.recipe.name}</h2>
+			<h4>
+				Tags:{' '}
+				{props.recipe.tags.map(tag => {
+					return <span>{tag} </span>;
+				})}
+			</h4>
 			<p>
-				<a href={props.recipe.url}>Check it out!</a>{' '}
+				<a href={props.recipe.url}>Take me to the Recipe!</a>{' '}
 				<a href={props.recipe.backup_url}>(backup link)</a>
 			</p>
 			<img src={props.recipe.image_url} alt={props.recipe.name} />
